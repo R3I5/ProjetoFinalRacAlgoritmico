d7eea18a27dd2ea0eb783ff090859f3a98ca946f
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-06-11 12:50:20 -0300

    Principais funções presentes no módulo produtos.py

--- modules/produtos.py
+++ modules/produtos.py
@@ -1,5 +1,5 @@
 #
-#Principais funções presentes no módulo produtos.py:
+#  Principais funções presentes no módulo produtos.py:
 #- Cadastro de novos produtos(Nome, Categoria, Preço, Estoque)
 #- Consulta de produtos
 #- Edição de informações de produtos existentes
