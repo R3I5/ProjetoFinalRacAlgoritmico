2c6b4ff790019172491c0bfac44004953f8c58ef
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-06-14 22:02:05 -0300

    Trabalho nas funções

---
+++ modules/usuarios.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+arquivo_usuarios = 'usuarios.json'
+
+def load_user():
+    return
+
+def save_user():
+    return
+
+def get_user():
+    return
+
+def validate_user():
+    return
